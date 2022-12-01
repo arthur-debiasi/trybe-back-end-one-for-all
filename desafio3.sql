fd8661715983e5ce0d314a6c952015b7e5fb95c8
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-12-01 09:52:26 -0300

    fix: format

--- desafio3.sql
+++ desafio3.sql
@@ -1,15 +1,12 @@
-SELECT
-	u.nome AS usuario,
+SELECT 
+    u.nome AS usuario,
     COUNT(h.cancao_id) AS qt_de_musicas_ouvidas,
     ROUND(SUM(c.duracao_segundos) / 60, 2) AS total_minutos
-    FROM SpotifyClone.usuarios AS u
-INNER JOIN 
-	SpotifyClone.historico_de_reproducoes AS h
-ON
-	u.id = h.usuario_id
-INNER JOIN
-	SpotifyClone.cancoes AS c
-ON
-	h.cancao_id = c.id
+FROM
+    SpotifyClone.usuarios AS u
+        INNER JOIN
+    SpotifyClone.historico_de_reproducoes AS h ON u.id = h.usuario_id
+        INNER JOIN
+    SpotifyClone.cancoes AS c ON h.cancao_id = c.id
 GROUP BY u.nome
 ORDER BY u.nome;
