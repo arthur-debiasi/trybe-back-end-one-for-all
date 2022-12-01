7b4c29cf55a4d63b08dbd2f0f6a52adb1ece221a
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-12-01 09:46:06 -0300

    feat: requisito 5

---
+++ desafio5.sql
@@ -0,0 +1,13 @@
+SELECT 
+	c.titulo_cancao AS cancao,
+    COUNT(h.cancao_id) AS reproducoes
+FROM 
+	SpotifyClone.cancoes AS c
+INNER JOIN
+	SpotifyClone.historico_de_reproducoes AS h
+ON
+	c.id = h.cancao_id
+GROUP BY
+	c.titulo_cancao
+ORDER BY reproducoes DESC, cancao ASC
+LIMIT 2;
