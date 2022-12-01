b5a293e160f7282524f9ad2ef6ad7dc74401d45e
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-12-01 09:52:13 -0300

    fix: format

--- desafio4.sql
+++ desafio4.sql
@@ -1,13 +1,11 @@
 SELECT 
-	u.nome AS usuario,
-    IF(YEAR(MAX(h.data_reproducao)) >= 2021, 'Usuário ativo', 'Usuário inativo') AS status_usuario
-FROM 
-	SpotifyClone.usuarios AS u
-INNER JOIN
-	SpotifyClone.historico_de_reproducoes AS h
-ON
-	u.id = h.usuario_id
-GROUP BY
-	u.nome
-ORDER BY
-	u.nome;
+    u.nome AS usuario,
+    IF(YEAR(MAX(h.data_reproducao)) >= 2021,
+        'Usuário ativo',
+        'Usuário inativo') AS status_usuario
+FROM
+    SpotifyClone.usuarios AS u
+        INNER JOIN
+    SpotifyClone.historico_de_reproducoes AS h ON u.id = h.usuario_id
+GROUP BY u.nome
+ORDER BY u.nome;
