efacbe6843bab83d09d2e72f2c4854304e13704b
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-12-01 09:48:16 -0300

    fix: fomatação

--- desafio5.sql
+++ desafio5.sql
@@ -1,13 +1,9 @@
 SELECT 
-	c.titulo_cancao AS cancao,
-    COUNT(h.cancao_id) AS reproducoes
-FROM 
-	SpotifyClone.cancoes AS c
-INNER JOIN
-	SpotifyClone.historico_de_reproducoes AS h
-ON
-	c.id = h.cancao_id
-GROUP BY
-	c.titulo_cancao
-ORDER BY reproducoes DESC, cancao ASC
+    c.titulo_cancao AS cancao, COUNT(h.cancao_id) AS reproducoes
+FROM
+    SpotifyClone.cancoes AS c
+        INNER JOIN
+    SpotifyClone.historico_de_reproducoes AS h ON c.id = h.cancao_id
+GROUP BY c.titulo_cancao
+ORDER BY reproducoes DESC , cancao ASC
 LIMIT 2;
