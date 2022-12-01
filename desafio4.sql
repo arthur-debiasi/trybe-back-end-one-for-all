fb0614a54a61533f9e1352adadc7e664300cfad0
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-12-01 08:17:14 -0300

    feat: req 4

---
+++ desafio4.sql
@@ -0,0 +1,13 @@
+SELECT 
+	u.nome AS usuario,
+    IF(YEAR(MAX(h.data_reproducao)) >= 2021, 'Usuário ativo', 'Usuário inativo') AS status_usuario
+FROM 
+	SpotifyClone.usuarios AS u
+INNER JOIN
+	SpotifyClone.historico_de_reproducoes AS h
+ON
+	u.id = h.usuario_id
+GROUP BY
+	u.nome
+ORDER BY
+	u.nome;
